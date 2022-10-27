b767d99027c6692b9219c49634db60a4569d7691
BojanSof <BojanSof@users.noreply.github.com>
2022-10-27 19:53:56 +0000

    deploy: 3b48daf5731b48bf0a270af96e9919808739eb81

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5bdb1f9e3a007658064d.js"
+    "url": "webpack-runtime-c41abc3070175845730a.js"
   },
   {
     "url": "styles.64213173aece1c6474c9.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "018931f818709feff30260387305dd2e"
+    "revision": "7b99acde9241ed63a7daae856bf8209f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-24f267f69d0223e60fc8.js"
@@ -51,7 +51,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "1309e351474707a8896dbbb7f2d0e641"
+    "revision": "d6a4cb784625f6e57d033ade770fb7c2"
   },
   {
     "url": "polyfill-08cbb2698ada64adcbfe.js"
